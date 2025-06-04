eb0d913e7c57752ced2b2a921ea4fd74a8fe9bee
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2025-06-04 11:53:58 +0900

    [ADD]Unity Project
    20250604 오전

--- Assets/98. Projects/Cat/Scripts/CatController.cs
+++ Assets/98. Projects/Cat/Scripts/CatController.cs
@@ -1,24 +1,39 @@
+using System;
 using UnityEngine;
+using Cat;
 
 public class CatController : MonoBehaviour
 {
+    public SoundManager soundManager;
     private Rigidbody2D catRb;
+    private Animator CatAnim;
     public float jumpPower = 10f;
     public bool isGround = false;
     public int jumpCount = 0;
-    
+
     void Start()
     {
         catRb = GetComponent<Rigidbody2D>();
+        CatAnim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        Jump();
+    }
+
+    void Jump()
+    {
         // 점프
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
         {
+            CatAnim.SetTrigger("Jump");
+            CatAnim.SetBool("isGround", false);
+
             catRb.AddForceY(jumpPower, ForceMode2D.Impulse);
             jumpCount++;
+
+            soundManager.OnJumpSound();
         }
     }
 
@@ -26,6 +41,8 @@ public class CatController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
+            CatAnim.SetBool("isGround", true);
+
             jumpCount = 0;
             isGround = true;
         }
---
+++ Assets/98. Projects/Cat/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Cat
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioSource audioSource;
+        public AudioClip bgmClip;
+        public AudioClip jumpClip;
+
+        void Start()
+        {
+            SetBGMSound();
+        }
+        
+        public void SetBGMSound()
+        {
+            audioSource.clip = bgmClip;
+            audioSource.playOnAwake = true;
+            audioSource.loop = true;
+
+            audioSource.Play();
+        }
+
+        public void OnJumpSound()
+        {
+            audioSource.PlayOneShot(jumpClip);
+        }
+    }
+}
