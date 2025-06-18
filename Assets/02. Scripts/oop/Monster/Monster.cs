2c1861c8c83abf749f80ca524e2d5b207862650b
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2025-06-18 16:48:01 +0900

    [ADD] Unity Project
    20250618

--- Assets/02. Scripts/oop/Monster/Monster.cs
+++ Assets/02. Scripts/oop/Monster/Monster.cs
@@ -1,22 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class Monster : MonoBehaviour
 {
+    public SpawnManager spawner;
+
     private SpriteRenderer sRenderer;
+    private Animator animator;
+
     protected float hp = 30f;
     protected float moveSpeed = 3f;
 
     public int dir = 1;
+    private bool isMove = true;
+    private bool isHit = false;
 
     public abstract void Init();
     void Start()
     {
+        spawner = FindFirstObjectByType<SpawnManager>();
+        
         sRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         Init();
     }
 
     void Update()
     {
+        Move();
+    }
+
+    void Move()
+    {
+        if (!isMove) return;
+
         transform.position += Vector3.right * dir * moveSpeed * Time.deltaTime;
 
         if (transform.position.x > 8f)
@@ -29,21 +46,43 @@ public abstract class Monster : MonoBehaviour
             dir = 1;
             sRenderer.flipX = false;
         }
+        
     }
 
     void OnMouseDown()
     {
-        Hit(1);
+        StartCoroutine(Hit(1));
     }
 
-    void Hit(float damage)
+    IEnumerator Hit(float damage)
     {
+        if (isHit) yield break;
+        
+        isHit = true;
+        isMove = false;
+
         hp -= damage;
 
         if (hp <= 0)
         {
+            animator.SetTrigger("Death");
             Debug.Log("die");
+
+            isHit = true;
+            isMove = false;
+
+            spawner.DropCoin(transform.position);
+
+            yield return new WaitForSeconds(3f);
             Destroy(gameObject);
+
+            yield break;
         }
+
+        animator.SetTrigger("Hit");
+
+        yield return new WaitForSeconds(0.5f);
+        isHit = false;
+        isMove = true;
     }
 }
---
+++ Assets/02. Scripts/oop/Monster/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] monsters;
+    [SerializeField] private GameObject coinPrefab;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            var randomIndex = Random.Range(0, monsters.Length);
+            var randomX = Random.Range(-8, 9);
+            var randomY = Random.Range(-3, 5);
+
+
+            var createPos = new Vector3(randomX, randomY, 0);
+
+            Instantiate(monsters[randomIndex], createPos, Quaternion.identity);
+        }
+    }
+
+    public void DropCoin(Vector3 dropPos)
+    {
+        Instantiate(coinPrefab, dropPos, Quaternion.identity);
+    }
+}
+
