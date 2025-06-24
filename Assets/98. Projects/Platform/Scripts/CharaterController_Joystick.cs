d34ff9bf95f23b1253c5121cb49b83f016614dfd
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2025-06-24 16:09:30 +0900

    [Add] Unity Project
    20250624

--- Assets/98. Projects/Platform/Scripts/CharaterController_Joystick.cs
+++ Assets/98. Projects/Platform/Scripts/CharaterController_Joystick.cs
@@ -1,23 +1,33 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharaterController_Joystick : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D CharRb;
 
+    [SerializeField] private Button jumpButton;
+    [SerializeField] private Button attackButton;
+
     private Vector3 inputDir;
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float jumpPower = 10f;
     private bool isGround = false;
+    private bool isAttack = false;
+    private bool isCombo = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         CharRb = GetComponent<Rigidbody2D>();
+
+        jumpButton.onClick.AddListener(Jump);
+        attackButton.onClick.AddListener(Attack);
     }
     void Update()
     {
-        
+
     }
 
     void FixedUpdate()
@@ -40,32 +50,54 @@ public class CharaterController_Joystick : MonoBehaviour
             isGround = false;
         }
     }
+    public void InputJoystick(float x, float y)
+    {
+        inputDir = new Vector3(x, y, 0).normalized;
 
+        animator.SetFloat("JoystickX", inputDir.x);
+        animator.SetFloat("JoystickY", inputDir.y);
+
+        if (inputDir.x != 0)
+        {
+            var scaleX = inputDir.x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(scaleX, 1, 1);
+        }
+    }
     void Move()
     {
         if (inputDir.x != 0)
             CharRb.linearVelocityX = inputDir.x * moveSpeed;
     }
-
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (isGround)
         {
             animator.SetTrigger("Jump");
             CharRb.AddForceY(jumpPower, ForceMode2D.Impulse);
         }
     }
-
-    void SetAnimation()
+    void Attack()
     {
-        if (inputDir.x != 0)
+        if (!isAttack)
         {
-            var scaleX = inputDir.x > 0 ? 1 : -1;
-            transform.localScale = new Vector3(scaleX, 1, 1);
-
-            animator.SetBool("isRun", true);
+            isAttack = true;
+            animator.SetTrigger("Attack");
         }
-        else if (inputDir.x == 0)
-            animator.SetBool("isRun", false);
+        else
+        {
+            isCombo = true;
+        }
+    }
+    public void CheckCombo()
+    {
+        if (isCombo)
+        {
+            animator.SetBool("isCombo", true);
+        }
+        else
+        {
+            animator.SetBool("isCombo", false);
+            isAttack = false;
+        }
     }
 }
--- Assets/98. Projects/Platform/Scripts/JoystickController.cs
+++ Assets/98. Projects/Platform/Scripts/JoystickController.cs
@@ -1,9 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class JoystickController : MonoBehaviour
+public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    public void OnLog(string msg)
+    [SerializeField] private CharaterController_Joystick joystickController;
+    [SerializeField] private GameObject backGroundUI;
+    [SerializeField] private GameObject handlerUI;
+
+    private Vector2 startPos, currPos;
+
+    void Start()
     {
-        Debug.Log(msg);
+        backGroundUI.SetActive(false);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        backGroundUI.SetActive(true);
+        backGroundUI.transform.position = eventData.position;
+        startPos = eventData.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        currPos = eventData.position;
+        Vector2 dragDir = currPos - startPos;
+
+        float maxDist = Mathf.Min(dragDir.magnitude, 75f);
+
+        handlerUI.transform.position = startPos + dragDir.normalized * maxDist;
+
+        joystickController.InputJoystick(dragDir.x, dragDir.y);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        joystickController.InputJoystick(0, 0);
+        handlerUI.transform.localPosition = Vector2.zero;
+        backGroundUI.SetActive(false);
     }
 }
