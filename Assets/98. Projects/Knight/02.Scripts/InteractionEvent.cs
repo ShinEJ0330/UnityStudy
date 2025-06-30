327725eef861afb7fca79c55b35fde1693740aa5
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2025-06-30 20:24:45 +0900

    [Add]
    20250630

---
+++ Assets/98. Projects/Knight/02.Scripts/InteractionEvent.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class InteractionEvent : MonoBehaviour
+{
+    public enum InteractionType { SIGN, DOOR, NPC }
+    public InteractionType type;
+
+    public SoundController soundController;
+
+    public GameObject popUp;
+
+    public FadeRoutine fade;
+    
+    public GameObject map;
+    public GameObject house;
+
+    public Vector3 inDoorPos;
+    public Vector3 outDoorPos;
+    public bool isHouse;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Interaction(other.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popUp.SetActive(false);
+        }
+    }
+
+    void Interaction(Transform player)
+    {
+        switch (type)
+        {
+            case InteractionType.SIGN:
+                popUp.SetActive(true);
+                break;
+            case InteractionType.DOOR:
+                StartCoroutine(DoorRoutine(player));
+                break;
+            case InteractionType.NPC:
+                popUp.SetActive(true);
+                break;
+        }
+    }
+
+    IEnumerator DoorRoutine(Transform player)
+    {
+        soundController.EventSoundPlay("Door Open");
+        
+        yield return StartCoroutine(fade.Fade(3f, Color.black, true));
+
+        map.SetActive(isHouse);
+        house.SetActive(!isHouse);
+
+        var pos = isHouse ? outDoorPos : inDoorPos; 
+        player.transform.position = pos;
+        
+        isHouse = !isHouse;
+        
+        soundController.EventSoundPlay("Door Close");
+        
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine(fade.Fade(3f, Color.black, false));
+
+    }
+}
